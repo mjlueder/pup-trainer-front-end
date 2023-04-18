9d2e28e7b8e160a59ee76c798aa355c053fd8e14
Mary Lueder <mlueder@gmail.com>
2023-04-18 17:46:02 -0400

    conditional links for hamburger menu

--- src/components/NavBar/SmallScreenNav.tsx
+++ src/components/NavBar/SmallScreenNav.tsx
@@ -21,30 +21,30 @@ const SmallScreenNav = (props: SmallScreenProps): JSX.Element => {
           <img src={DogHouse} alt="home button dog house" className={styles.homeIcon}/>
         </NavLink>
 
-        <div>
+        <div className={styles.menu}>
           <button onClick={handleOpen}>
             {isOpen ? 'x' : '≡'}
           </button>
 
           <div>
             
-          {user ?
-            <ul >
-              <li><NavLink to="/about">About</NavLink></li>
-              <li><NavLink to="/profile">My Dogs</NavLink></li>
-              <li><NavLink to="/dogs">All Dogs</NavLink></li>
-              <li><NavLink to="/resources">Training Resources</NavLink></li>
-              <li><NavLink to="" onClick={handleLogout}>Logout</NavLink></li>
-            </ul>
-          :
-            <ul >
-              <li><NavLink to="/about">About</NavLink></li>
-              <li><NavLink to="/resources">Training Resources</NavLink></li>
-              <li><NavLink to="/dogs">All Dogs</NavLink></li>
-              <li><NavLink to="/login" >Login</NavLink></li>
-              <li><NavLink to="/signup">Sign Up</NavLink></li>
-            </ul>
-          }
+            {isOpen && (user ?
+              <ul >
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/profile">My Dogs</NavLink></li>
+                <li><NavLink to="/dogs">All Dogs</NavLink></li>
+                <li><NavLink to="/resources">Training Resources</NavLink></li>
+                <li><NavLink to="" onClick={handleLogout}>Logout</NavLink></li>
+              </ul>
+            :
+              <ul >
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/resources">Training Resources</NavLink></li>
+                <li><NavLink to="/dogs">All Dogs</NavLink></li>
+                <li><NavLink to="/login" >Login</NavLink></li>
+                <li><NavLink to="/signup">Sign Up</NavLink></li>
+              </ul>
+            )}
 
           </div>
         </div>
